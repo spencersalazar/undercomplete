326e22698214315bb8e61456f97c8dc4b9346374
Spencer Salazar <spencer.salazar@gmail.com>
2013-02-20 05:26:58 -0800

    windowed overlap add

--- oc.m
+++ oc.m
@@ -1,14 +1,19 @@
 % oc.m
 
 addpath('GPSR_6.0');
-addpath('lasso');
-addpath('lasso/sub');
 
-[y fs] = wavread('ohno.wav');
+[y fs] = wavread('al.wav');
+y = y(:,1); % take first channel
 
+
+block_size = 1024;
+hop_size = block_size/2;
+window = hanning(block_size);
+
+tic
 % construct basis
 % sawtooths of varying frequency
-Nb = 1024; % basis length
+Nb = block_size; % basis length
 Jb = 100; % number of basis functions
 B = zeros(Jb, Nb);
 % construct basis by dividing 0-nyquist into equal parts
@@ -17,19 +22,20 @@ for jj=1:Jb
 	t = (0:Nb-1)/fs;
 	B(jj,:) = [1-2*fmod(t*freq,1)];
 end
+toc
 
-y = y(1:Nb,1); % take first Nb samples of first channel
-
-%[w A] = lars(B,y,'lasso');
-% 
-% [w resnorm residual exitflag] = lsqnonneg(B',y');
-% for ii = 1:1000
-% 	[w resnorm residual exitflag] = lsqnonneg(B',y', w);
-% end
-
-w = GPSR_BB(y,B',1);
-
-y_re = B'*w;
+% pad out to hop size
+y = [y; zeros(hop_size - mod(length(y),hop_size),1)];
+y_re = zeros(length(y),1);
+n = 1;
+tic
+while n+block_size-1 <= length(y)
+	w = GPSR_BB(y(n:n+block_size-1) .* window, B', 1, 'Verbose', 0);
+	y_re(n:n+block_size-1) = y_re(n:n+block_size-1) + (B'*w);
+	
+	n = n + hop_size;
+end
+toc
 
 wavwrite(y_re, fs, 'constrained.wav');
 wavwrite(y, fs, 'orig.wav');
